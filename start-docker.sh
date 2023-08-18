2a3f292b09567dd1a098918086aa9f859c33a17e
contributor <foss.contributor@gmail.com>
2023-08-18 15:54:34 +0300

    delete USER_GID before trying to change node group id

--- start-docker.sh
+++ start-docker.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 [[ ! -z "${USER_UID}" ]] && usermod -u ${USER_UID} node || echo "No USER_UID specified, leaving 1000"
-[[ ! -z "${USER_GID}" ]] && groupmod -g ${USER_GID} node || echo "No USER_GID specified, leaving 1000"
+
+if [[ ! -z "${USER_GID}" ]]; then
+    echo "USER_GID is not empty. Its value is: ${USER_GID}"
+    groupdel $(getent group ${USER_GID} | cut -d ':' -f 1) --force
+    groupmod -g ${USER_GID} node || echo "No USER_GID specified, leaving 1000"
+fi
 
 chown -R node:node /home/node
 exec su-exec node node ./src/www

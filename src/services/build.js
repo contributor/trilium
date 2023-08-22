ed64b6ff207aefdb4e7f8b25245e308ee40e79ac
contributor <foss.contributor@gmail.com>
2023-08-23 01:07:22 +0300

    local release 0.60.52

--- src/services/build.js
+++ src/services/build.js
@@ -1,1 +1,1 @@
-module.exports = { buildDate:"2023-06-19T23:26:50+02:00", buildRevision: "5905950c17791ce0eb278e010c2c8b3450fdb447" };
+module.exports = { buildDate:"2023-08-23T00:26:50+02:00", buildRevision: "cea4de1fd919834028ba92c32e109fe6f802a0a6" };

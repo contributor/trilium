6890c2a2a6d24f1c97343fc2cae24d16fdb2e421
contributor <foss.contributor@gmail.com>
2025-12-06 01:52:08 +0200

    fix: Handle null/undefined iframeOptions with || '' #337

--- apps/client/src/widgets/type_widgets/WebView.tsx
+++ apps/client/src/widgets/type_widgets/WebView.tsx
@@ -24,7 +24,7 @@ interface WebViewContentProps {
 
 function WebViewContent({ src, iframeOptions }: WebViewContentProps) {
     if (!isElectron) {
-        return <iframe src={src} class="note-detail-web-view-content" sandbox={`allow-same-origin allow-scripts allow-popups ${iframeOptions}`} />
+        return <iframe src={src} class="note-detail-web-view-content" sandbox={`allow-same-origin allow-scripts allow-popups ${iframeOptions || ''}`} />
     } else {
         return <webview src={src} class="note-detail-web-view-content" />
     }
